676f8553fe10f593585a85971fa5d6b1a565cd54
ANIKET SINDHU <57598532+AniketSindhu@users.noreply.github.com>
2022-01-31 09:03:30 +0530

    added fa2 functions

--- Swap/swap.py
+++ Swap/swap.py
@@ -1,5 +1,41 @@
 import smartpy as sp
 
+class ContractLibrary(sp.Contract):
+    
+    def TransferFATwoTokens(sender,reciever,amount,tokenAddress,id):
+
+        arg = [
+            sp.record(
+                from_ = sender,
+                txs = [
+                    sp.record(
+                        to_         = reciever,
+                        token_id    = id , 
+                        amount      = amount 
+                    )
+                ]
+            )
+        ]
+
+        transferHandle = sp.contract(
+            sp.TList(sp.TRecord(from_=sp.TAddress, txs=sp.TList(sp.TRecord(amount=sp.TNat, to_=sp.TAddress, token_id=sp.TNat).layout(("to_", ("token_id", "amount")))))), 
+            tokenAddress,
+            entry_point='transfer').open_some()
+
+        sp.transfer(arg, sp.mutez(0), transferHandle)
+
+    def Mint(amount, reciever, tokenAddress,id):
+        
+        arg = sp.record(address = reciever, amount = amount, token_id = id, metadata = sp.map(l={}))
+        
+
+        transferHandle = sp.contract(
+            sp.TRecord(address=sp.TAddress, amount = sp.TNat, token_id = sp.TNat, metadata = sp.TMap(sp.TString, sp.TBytes)), 
+            tokenAddress,
+            entry_point='mint').open_some()
+
+        sp.transfer(arg, sp.mutez(0), transferHandle)
+        
 class Swap(sp.Contract):
     def __init__(self, _oldTokenAddress, _newTokenAddress, admin): 
         self.init(
