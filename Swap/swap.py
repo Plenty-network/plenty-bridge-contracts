2aaef441bb4b19f371ad9b570137aa563ad8e6d6
ANIKET SINDHU <57598532+AniketSindhu@users.noreply.github.com>
2022-02-23 02:10:21 +0530

    Update swap.py

--- Swap/swap.py
+++ Swap/swap.py
@@ -45,11 +45,29 @@ class Swap(sp.Contract):
             locked = False
         )
 
+    def getTokenId(self, tokenId):
+        newTokenid = sp.local('newTokenid', 100)
+        sp.if tokenId == 1:
+            newTokenid.value = 0 #busd
+        sp.if tokenId == 11:
+            newTokenid.value = 1 #matic
+        sp.if tokenId == 17:
+            newTokenid.value = 2 #usdc  
+        sp.if tokenId == 18:
+            newTokenid.value = 3 #usdt
+        sp.if tokenId == 19:
+            newTokenid.value = 4 #btc
+        sp.if tokenId == 20:
+            newTokenid.value = 5 #eth
+        return newTokenid.value
+
     @sp.entry_point
     def swapTokens(self,params):
         sp.set_type(params, sp.TRecord(tokenId = sp.TNat, amount = sp.TNat))
+        newTokenId = sp.local('newTokenId', self.getTokenId(params.tokenId))
+        sp.verify(newTokenId.value != 100, "ErrorMessage.AssetNotSwapable")
         ContractLibrary.TransferFATwoTokens(sp.sender, sp.self_address, params.amount, self.data.oldTokenAddress, params.tokenId)
-        ContractLibrary.Mint(params.amount, sp.sender, self.data.newTokenAddress, params.tokenId)
+        ContractLibrary.Mint(params.amount, sp.sender, self.data.newTokenAddress, newTokenId.value)
 
     @sp.entry_point
     def setAddress(self,params):
