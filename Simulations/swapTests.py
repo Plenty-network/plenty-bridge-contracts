af26ad25c358d011753c6ea050bf3ec5bf8bcbdd
ANIKET SINDHU <57598532+AniketSindhu@users.noreply.github.com>
2022-04-04 13:04:52 +0530

    updated tests/simulations

--- Simulations/swapTests.py
+++ Simulations/swapTests.py
@@ -24,7 +24,7 @@ if "templates" not in __name__:
         minter1 = sp.test_account("Minter1")
         minter2 = sp.test_account("Minter2")
         
-        config = FA2_TOKEN.FA2_config(
+        config = FA2_config(
                 debug_mode = global_parameter("debug_mode", False),
                 single_asset = global_parameter("single_asset", False),
                 non_fungible = global_parameter("non_fungible", False),
@@ -42,12 +42,17 @@ if "templates" not in __name__:
         scenario = sp.test_scenario()
 
         scenario.h1("Initializing swap")
-        c1 = SWAP.Swap(_oldTokenAddress = alice.address, _newTokenAddress = alice.address, admin = admin.address)
+        c1 = Swap(
+            _oldTokenAddress = sp.address("KT1VoHhkb6wXnoPDvcpbnPFYGTcpJaPfRoEh"),
+            _newTokenAddress = sp.address("KT1EZBn43coqL6xfT5xL6e49nhEPLp9B8m4n"),
+            admin = admin.address,
+            _tokenMapping = {1: 0, 11: 1, 17: 2, 18: 3, 19: 4, 20: 5, 10: 6, 5: 7}
+        )
 
         scenario += c1
 
         scenario.h1("Creating old token")
-        oldToken = FA2_TOKEN.FA2(config = config,
+        oldToken = FA2(config = config,
                  metadata = sp.utils.metadata_of_url("https://example.com"),
                  admin = admin.address,
                  minter1 = minter1.address,
@@ -55,7 +60,7 @@ if "templates" not in __name__:
         scenario += oldToken
 
         scenario.h1("Creating new token")
-        newToken = FA2_TOKEN.FA2(config = config,
+        newToken = FA2(config = config,
                  metadata = sp.utils.metadata_of_url("https://example.com"),
                  admin = admin.address,
                  minter1 = minter1.address,
@@ -66,26 +71,46 @@ if "templates" not in __name__:
         c1.setAddress(sp.record(oldTokenAddress = oldToken.address, newTokenAddress = newToken.address)).run(sender = admin)
 
         scenario.h1("Minting/Creating old token id = 0")
-        oldTok1_md = FA2_TOKEN.FA2.make_metadata(
-            name = "wUSDT",
-            decimals = 6,
-            symbol= "wUSDT")
+        oldTok1_md = FA2.make_metadata(
+            name = "wAave",
+            decimals = 18,
+            symbol= "wAave")
         oldToken.mint(address = bob.address,
                             amount = 100,
                             metadata = oldTok1_md,
                             token_id = 0).run(sender = minter1)
 
+        scenario.h1("Minting/Creating old token id = 1")
+        oldTok2_md = FA2.make_metadata(
+            name = "wUSDT",
+            decimals = 18,
+            symbol= "wUSDT")
+        oldToken.mint(address = bob.address,
+                            amount = 100,
+                            metadata = oldTok2_md,
+                            token_id = 1).run(sender = minter1)
+
         scenario.h1("Minting/Creating new token id = 0")
-        newTok1_md = FA2_TOKEN.FA2.make_metadata(
+        newTok1_md = FA2.make_metadata(
             name = "USDT.e",
             decimals = 6,
             symbol= "USDT.e")
         newToken.mint(address = admin.address,
-                            amount = 100,
+                            amount = 0,
                             metadata = newTok1_md,
                             token_id = 0).run(sender = minter1)
 
-        scenario.h1("Setting Swap as opreator for bob's old token")
+        scenario.h1("Minting/Creating new token id = 1")
+        newTok2_md = FA2.make_metadata(
+            name = "UAave.e",
+            decimals = 6,
+            symbol= "UAave.e")
+        newToken.mint(address = admin.address,
+                            amount = 0,
+                            metadata = newTok2_md,
+                            token_id = 1).run(sender = minter1)
+
+        scenario.h1("Setting Swap as opreator for bob's old token --> 0")
         oldToken.update_operators([
                 sp.variant("add_operator", oldToken.operator_param.make(
                     owner = bob.address,
@@ -94,8 +119,20 @@ if "templates" not in __name__:
                 ))
             ]).run(sender = bob, valid = True)
 
-        scenario.h1("Swapping old token for new one")
-        c1.swapTokens(sp.record(tokenId = 0, amount = 50)).run(sender = bob)
+        scenario.h1("Setting Swap as opreator for bob's old token --> 1")
+        oldToken.update_operators([
+                sp.variant("add_operator", oldToken.operator_param.make(
+                    owner = bob.address,
+                    operator = c1.address,
+                    token_id = 1
+                ))
+            ]).run(sender = bob, valid = True)
 
         scenario.h1("Swapping old token for new one")
+        c1.swapTokens(sp.record(tokenId = 1, amount = 50)).run(sender = bob)
+
+        scenario.h1("Adding mapping")
+        c1.addMapping(sp.record(oldTokenId = 0, newTokenId = 1)).run(sender = admin)
+
+        scenario.h1("Swapping old token for new one --> 0")
         c1.swapTokens(sp.record(tokenId = 0, amount = 50)).run(sender = bob)
